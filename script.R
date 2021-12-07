b65f3a3cb2c8fd5e3bbfdf30804747ec6546ae03
PBlanca <pamelablanca.bl@gmail.com>
2021-12-07 10:24:04 -0300

    Update script

--- script.R
+++ script.R
@@ -1,7 +1,27 @@
 #########################
-# Loading Iris dataframe
+# Loading Iris Data Frame
 #########################
 
 library(tidyverse)
 library(RCurl)
+library(skimr)
+
 iris <- read.csv(text = getURL("https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv") )
+
+### Explore data
+
+head(iris)
+summary(iris)
+
+sum(is.na(iris))
+skim(iris)
+
+# Group by species
+iris %>% 
+  group_by(Species) %>% 
+  skim() 
+
+### First data visualization 
+
+
+
